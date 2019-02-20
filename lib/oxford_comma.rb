d4390cc249117df477aeba36a003d7c98731c4eb
Jeffrey Yu <jwy5140@gmail.com>
2019-02-20 20:49:02 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,6 +2,6 @@ def oxford_comma(array)
   if array.length == 1 
     return array[0]
   else 
-    return (array[0...array.length-1].join(", ")).concat(" and #{array.length")
+    return (array[0...array.length-1].join(", ")).concat(" and #{array.last}")
   end
 end
